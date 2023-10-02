bda5ec2cc7b00219b06e82dd6daff54a3a2733c6
kthss01 <kthss01@naver.com>
2023-10-02 20:38:18 +0900

    directional - 방향탄

---
+++ algorithm/danmaku/variation/1_spiral/directional/js/Bullets.js
@@ -0,0 +1,66 @@
+class Bullet extends Phaser.Physics.Arcade.Sprite {
+	constructor(scene, x, y) {
+		super(scene, x, y, "bullet");
+
+		this.scene = scene;
+	}
+
+	fire(x, y, angle, speed = 300) {
+		this.body.reset(x, y);
+
+		this.setActive(true);
+		this.setVisible(true);
+
+		this.scene.physics.velocityFromAngle(angle, speed, this.body.velocity);
+		// this.scene.physics.velocityFromRotation(angle, speed, this.body.velocity); // radian으로 회전
+	}
+
+	preUpdate(time, delta) {
+		super.preUpdate(time, delta);
+
+		if (this.y <= -32) {
+			this.setActive(false);
+			this.setVisible(false);
+		}
+	}
+}
+
+export default class Bullets extends Phaser.Physics.Arcade.Group {
+	constructor(scene) {
+		super(scene.physics.world, scene);
+
+		this.createMultiple({
+			frameQuantity: 500,
+			key: "bullet",
+			active: false,
+			visible: false,
+			classType: Bullet,
+		});
+	}
+
+	fireBullet(x, y, angle) {
+		const bullet = this.getFirstDead(false);
+
+		if (bullet) {
+			bullet.fire(x, y, angle);
+		}
+	}
+
+	danmakuFire = () => {
+		// 시간에 따른 발사
+		this.scene.time.addEvent({
+			delay: 100,
+			startAt: 100,
+			repeat: this.getLength() - 1,
+			callback: () => {
+				const x = 300;
+
+				this.fireBullet(x, -25, 90);
+			},
+		});
+	};
+
+	getRandomInt = (max) => {
+		return Math.floor(Math.random() * max);
+	};
+}
